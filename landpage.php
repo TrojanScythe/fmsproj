0a67690320813658c7755ad3718909003dd84f72
TrojanScythe <marquezianernesto@gmail.com>
2026-03-04 15:39:54 +0800

    ytest

--- landpage.php
+++ landpage.php
@@ -1,10 +1,14 @@
+<?php
+    echo '<link rel="stylesheet" href="cssshi/test.css">';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arellano FMS</title>
-    <link rel="stylesheet" href="/cssshi/test.css">
+    <link rel="stylesheet" href="cssshi/test.css"></link>
 </head>
 <body>
 <div class="navbar">
